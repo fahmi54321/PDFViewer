8240623a5ebc457952370caa04c978e77d17e973
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-09 15:51:38 +0700

    Initial commit

---
+++ app/src/main/java/com/android/a56pdfviewer/MainActivity.java
@@ -0,0 +1,56 @@
+package com.android.a56pdfviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    PDFView pdfView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pdfView = findViewById(R.id.pdfView);
+//        pdfView.fromAsset("pdf.pdf").load(); //This is function read PDF from assets
+
+
+        //read pdf from url
+        new RetrivePDFStream().execute("https://www.tutorialspoint.com/kotlin/kotlin_tutorial.pdf");
+    }
+
+    class RetrivePDFStream extends AsyncTask<String,Void, InputStream>{
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inputStream = null;
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                if (urlConnection.getResponseCode() == 200){
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                }
+            }catch (IOException e){
+                return null;
+            }
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            pdfView.fromStream(inputStream).load();
+        }
+    }
+}
